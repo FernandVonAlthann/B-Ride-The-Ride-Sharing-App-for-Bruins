17c2050689268243db7cedeb2385268b47d3179e
Fernando Von Althann <159071708+FernandVonAlthann@users.noreply.github.com>
2025-02-10 22:25:44 -0800

    Update page.tsx

--- src/app/forum/page.tsx
+++ src/app/forum/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import "./globals.css";
 import { useRouter } from "next/navigation"; // Import router
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
