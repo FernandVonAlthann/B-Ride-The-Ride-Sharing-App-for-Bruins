f238023c86bac2b18b469ffb622eaa86fbf19719
Fernando Von Althann <159071708+FernandVonAlthann@users.noreply.github.com>
2025-02-18 12:45:36 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ThemeProvider } from "@/app/ThemeProvider";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -19,11 +20,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="h-full">
-      <body className="h-full bg-transparent">
-        {children}
+    <html lang="en">
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );
 }
-
