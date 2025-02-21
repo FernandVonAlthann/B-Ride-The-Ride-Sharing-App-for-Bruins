e648bf78773e84d77b5b9371946355683b3b0ad6
Fernando Von Althann <159071708+FernandVonAlthann@users.noreply.github.com>
2025-02-21 10:39:11 -0800

    Delete src directory

--- src/components/ui/input.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-export const Input: React.FC<InputProps> = ({ className, ...props }) => {
-  return (
-    <input {...props} className={className} />
-  );
-};
