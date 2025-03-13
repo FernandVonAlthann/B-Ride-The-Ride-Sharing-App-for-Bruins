5270eebe83717cdc711d7275e30693b0912e2299
N3ilPal <23palneil@gmail.com>
2025-03-12 18:38:53 -0700

    Changed the thickness for higher contrast on dark blue button background

--- src/app/group-chat/page.tsx
+++ src/app/group-chat/page.tsx
@@ -130,7 +130,7 @@ export default function GroupChat() {
                   className="p-6 bg-gradient-to-r from-[#0F172A] to-[#1E40AF] hover:from-[#172554] hover:to-[#2563EB] text-white shadow-md rounded-[20px]"
                   onClick={sendMessage}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
                     <line x1="22" y1="2" x2="11" y2="13"></line>
                     <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
                   </svg>
